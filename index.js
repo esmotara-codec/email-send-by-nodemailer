0d1601cc9c9f6b5662157b3e17ccdc2aba98fbc2
esmotara-codec <esmotara1725@gmail.com>
2025-09-06 22:06:55 +0600

    email transporter set

--- index.js
+++ index.js
@@ -17,16 +17,50 @@ const emailTransporter = nodemailer.createTransport({
   service : "gmail",
   auth : {
     user : process.env.Zap_Email, 
+    pass : process.env.Zap_Pass,
   }
 })
 console.log(process.env.Zap_Email);
 
+//Email route
 app.get("/send-payment-email", async(req, res) => {
+ const  paymentInfo = {
+    transactionId : "aaaaaaaa",
+    user: "sohagali.ru.ac@gmail.com",
+    parcelInfo : "20kg mangoo "
 
-  const  paymentInfo = {
-    transactionId : "",
-    user: "esmotaramonisa1002@gmail.com",
   }
+  const emailObject = {
+    from : `"Zap Email Service" <${process.env.Zap_Email}>`,
+    to : paymentInfo.user,
+    subject: "Zap payment delivary confirmation",
+    html: `
+    <h2>Payment confirmation</h2>
+    <p>Thank you for the payment , We have recieved your delivary payment </p>
+    <br/>
+    <h3> Transaction ID : ${paymentInfo.transactionId} </h3>
+    <p>If you face any issue please contact with our support team</p>
+    <button>Click here </button>
+    
+    `
+  }
+
+  try {
+      const emailInfo = await emailTransporter.sendMail(emailObject);
+      console.log("email sent: ", emailInfo.messageId);
+      res.send({result: 'sucess'})
+  
+      
+    } catch (error) {
+      
+      console.log("email send error : ", error);
+      res.send({result: 'email sent fail'})
+  
+
+      
+    }
+
+
 })
 
 app.post("/send-email", async (req, res) => {
